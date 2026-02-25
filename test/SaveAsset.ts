390919d79ebb37b25fc2b1f06c9de7350c6097e2
Feyisara2108 <ayokomi2108@gmail.com>
2026-02-25 15:59:00 +0100

    test: update test for save asset contract

--- test/SaveAsset.ts
+++ test/SaveAsset.ts
@@ -32,21 +32,40 @@ describe("SaveAsset", function () {
 		expect(await saveAsset.connect(addr1).getUserSavings()).to.eq(depositAmount);
 	});
 
+	it("should not be able to deposit 0 ether in save asset contract", async function () {
+    await expect(
+      saveAsset.connect(addr2).deposit({ value: 0 }),
+    ).to.be.revertedWith("Can't deposit zero value");
+    });
 
+	it("should be able to withdraw ether in save asset contract", async function () {
+		const withdrawAmount = ethers.parseEther("3");
+		await saveAsset.connect(addr2).deposit({ value : withdrawAmount});
+		expect(await saveAsset.connect(addr2).getUserSavings()).to.eq(withdrawAmount);
+	});
 
+	it ("should not be able to withdraw 0 ether in save asset contract", async function () {
+	await expect(
+		saveAsset.connect(addr2).deposit({ userSavings : 0}),).to.be.revertedWith("Can't deposit zero value");
+	});
 
+	it ("should be able to get user savings", async function (){
+		const getUserSavings = ethers.parseEther("1");
+		await saveAsset.connect(addr1).getUserSavings({ amount: getUserSavings});
+		expect(await saveAsset.connect(addr1).getUserSavings()).to.eq(0);
+	});
 
+	it ("should be able to get contract balance", async function () {
+		const getContractBalance = ethers.parseEther("2");
+		await saveAsset.connect(addr2).getContractBalance({amount: getContractBalance});
+		expect(await saveAsset.connect(addr2).getContractBalance()).to.eq(0);
+	});
 
-
-
-
-
-
-
-
-
-
-
+	it("should be able to deposit ERC20 token ether in save asset contract", async function(){
+        const depositERC20 = ethers.parseEther("2");
+		await saveAsset.connect(addr3).depositERC20({ amount: depositERC20});
+		expect (await saveAsset.connect(addr3).depositERC20()).to.eq(depositERC20);
+	});
 
 
 
